499f1e6b111fc39f089819b1ae03705a1918e599
fhabhishektiwari <abhishektiwarifh@gmail.com>
2022-05-11 14:50:31 +0530

    added code in php file

--- formvalidation.php
+++ formvalidation.php
@@ -1,5 +1,49 @@
 <?php
-    $nameErr=$emailErr=$websiteErr=$genderErr='';
+    $nameErr=$emailErr=$websiteErr=$genderErr=$commentErr='';
+    $name=$email=$website=$gender=$comment='';
+
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+        
+        if(empty($_POST['name'])){
+            $nameErr="Name is required";
+        }else{
+            $name=test_input($_POST['name']);
+        }
+        
+        if(empty($_POST['email'])){
+            $emailErr="Email is required";
+        }else{
+            $email=test_input($_POST['email']);
+        }
+            
+        if(empty($_POST['website'])){
+            $websiteErr="";
+        }else{
+            $website=test_input($_POST['website']);
+        }
+        
+        if(empty($_POST['comment'])){
+            $commentErr="";
+        }else{
+            $comment=test_input($_POST['comment']);
+        }
+        
+        if(empty($_POST['gender'])){
+            $genderErr="Gender is required";
+        }else{
+            $gender=test_input($_POST['gender']);
+        }
+        
+    }
+
+    function test_input($data){
+        $data=trim($data);
+        $data=stripslashes($data);
+        $data=htmlspecialchars($data);
+        return $data;
+    }
+
+
 
 ?>
 
@@ -52,25 +96,41 @@
                 outline: none;
                 cursor: pointer;
             }
+            
+            .error{
+                color: #f00;
+            }
+            
+            .output{
+                border: 2px solid #fff;
+                width: 450px;
+                height: 450px;
+                margin: 50px auto;
+                padding: 25px;
+            }
         </style>
     </head>
     
     <body>
         <h2>Form Validation</h2>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
             <label>Name:</label>
-            <input type="text" name="name" />
             <span class="error">*<?php echo  $nameErr;?></span>
+            <input type="text" name="name" />
+            
             <label>Email</label>
-            <input type="email" name="email" />
             <span class="error">*<?php echo  $emailErr;?></span>
+            <input type="email" name="email" />
+            
             <label>Website</label>
-            <input type="text" name="website" />
             <span class="error">*<?php echo  $websiteErr;?></span>
+            <input type="text" name="website" />
+           
             <label>Comment:</label> 
             <textarea name="comment" rows="5" cols="40"></textarea>
             <br><br>
             <lable>Gender</lable>
+             <span class="error">*<?php echo  $genderErr;?></span><br>
             <input type="radio" name="gender" value="Male"><label>Male</label>
             <input type="radio" name="gender" value="Female"><label>Female</label>
             <input type="radio" name="gender" value="Other"><label>Other</label>
@@ -78,6 +138,17 @@
             <br><br>
             <input type="submit" value="submit" name="submit">
         </form>
+        
+        <?php
+            echo '<div class="output">'. 
+            "<h3>Your Inputs:</h1>".
+            '<p>Name: '.$name.'</p>'.
+            '<p>Email: '.$email.'</p>'.
+            '<p>Website Name: '.$website.'</p>'.
+            '<p>Gender: '.$gender.'</p>'.
+            '<p>Your Comment: '.$comment.'</p>';
+        
+        ?>
     
     </body>
 </html>
